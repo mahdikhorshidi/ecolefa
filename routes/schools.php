00ee05f538dae385690910b9ead927a7b1760259
Mahdi Khorshidi <0603238556@markazi-its.gov>
2018-10-02 11:24:19 +0330

    first commit

--- routes/schools.php
+++ routes/schools.php
@@ -1,6 +1,6 @@
 <?php
 
-/*Route::get('/', function (\App\Models\School $school,$domain,$tld = null) {
+Route::get('/', function (\App\Models\School $school,$domain,$tld = null) {
     if (!isset($school->name)) {
         $school = \App\Models\School::where('domain', $domain . '.' . $tld)->first();
     }
@@ -8,8 +8,8 @@
         abort(404);
     }
     return view('schools/welcome', compact('school'));
-});*/
-Route::get('/', function () {
-
-    return \App\Models\School::school()->first();
 });
+// Route::get('/', function () {
+
+//     return \App\Models\School::school()->first();
+// });
